6c81c66ca9d3ddcb8fdaf1e770e3613a74c207f4
nooramaj <155659085+nooramaj@users.noreply.github.com>
2026-08-07 02:25:39 +0300

    Change base path in Vite config to './'

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
         },
       },
       plugins: [react()],
-      base: "/",
+      base: "./",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
